7e2e8683bf1425f1a68879970de192846b362a55
Matthew Neilson <2322473N@student.gla.ac.uk>
2019-01-21 10:23:07 +0000

    Chapter 4 Completed

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -19,8 +19,13 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '#)_o1jz8(ft+i$&q+6c-u##w2^xxa9(1bl6397qx$vyb#y4&j!'
@@ -66,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
